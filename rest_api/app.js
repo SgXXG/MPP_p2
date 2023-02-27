d8e58a14cbcc114df1843efe1950eedc352fa7fd
Stanislav <72697394+SgXXG@users.noreply.github.com>
2023-02-27 11:46:07 +0300

    update

--- rest_api/app.js
+++ rest_api/app.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
 
+let NOTES = [{id: v4(), title: 'Note', desc: 'first note', marked: false}]
 
+app.use(express.json())
+
+//GET
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(NOTES)
+})
+
+//POST
+app.post('/api/notes', (req, res) => {
+    const note = {...req.body, id: v4(), marked: false}
+    NOTES.push(note)
+    res.status(201).json(note)
+})
+
+//DELETE
+app.delete('/api/notes/:id', (req, res) => {
+    NOTES = NOTES.filter(n => n.id !== req.params.id)
+    res.status(200).json({message: 'Note was successfully deleted'})
+})
+
+//PUT
+app.put('/api/notes/:id', (req, res) => {
+    const idx = NOTES.findIndex(n => n.id === req.params.id)
+    NOTES[idx] = req.body
+    res.status(200).json(NOTES[idx])
+})
 
 
 app.use(express.static(path.resolve(__dirname, 'client')))
--- rest_api/client/front.js
+++ rest_api/client/front.js
@@ -1,32 +1,82 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
+Vue.component('loader', {
+    template: `
+        <div style="display: flex; justify-content: center; align-items: center">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    `
+})
+
 new Vue({
     el: '#app',
     data() {
         return{
+            loading: false,
             form: {
                 title: '',
                 desc: ''
             },
-            notes: [
-                {id: 1, title: 'Note', desc: 'first note', marked: false}
-            ]
+            notes: []
+        }
+    },
+    computed: {
+        canCreate() {
+            return this.form.title.trim() && this.form.desc.trim()
         }
     },
     methods: {
-        createNote() {
+        async createNote() {
             const {...note} = this.form
 
-            this.notes.push({...note, id: Date.now()})
+            const newNote = await request('/api/notes', 'POST', note)
+
+            this.notes.push(newNote)
 
             this.form.title = this.form.desc = ''
         },
-        markNote(id) {
+        async markNote(id) {
             const note = this.notes.find(n => n.id === id)
-            note.marked = true
-        },
-        removeNote(id) {
 
+            const updated = await request(`/api/notes/${id}`, 'PUT', {
+                ...note,
+                marked: true
+            })
+
+            note.marked = updated.marked
+        },
+        async removeNote(id) {
+            await request(`api/notes/${id}`, 'DELETE')
+            this.notes = this.notes.filter(n => n.id !== id)
+            alert('Note was successfully deleted')
         }
+    },
+    async mounted() {
+        this.loading = true
+        this.notes = await request('/api/notes')
+        this.loading = false
     }
 })
+
+async function request(url, method='GET', data=null) {
+    try{
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch(e) {
+        console.warn('Error', e.message)
+    }
+}
